becb7adb9e40895dee89ef75a8f668e6f53029b5
Matthew A Rosales <matthewrosales@usf.edu>
2024-05-21 20:50:14 -0400

    Add accelerate method to increase speed by 10 km/h.

--- cc_3.js
+++ cc_3.js
@@ -7,3 +7,9 @@ class Car {
     this.speed = speed;
   }
 
+// Implementing an acceleration method that increases the car speed by 10 km/h
+accelerate() {
+  this.speed += 10;
+  console.log(`${this.make} speed increased to ${this.speed} km/h`);
+}
+  
